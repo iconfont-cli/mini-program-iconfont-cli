ee66468985ed56dddef3adb88b0ac06aa3e4e602
zhengjie05 <zhengjie05@kuaishou.com>
2021-07-15 16:58:30 +0800

    feat: support kuaishou app

--- scripts/update-snapshot.sh
+++ scripts/update-snapshot.sh
@@ -2,6 +2,7 @@
 
 npx ts-node src/commands/createQqIcon.ts --config ./scripts/config/qq.json
 npx ts-node src/commands/createToutiaoIcon.ts --config ./scripts/config/toutiao.json
+npx ts-node src/commands/createKuaishouIcon.ts --config ./scripts/config/kuaishou.json
 npx ts-node src/commands/createBaiduIcon.ts --config ./scripts/config/baidu.json
 npx ts-node src/commands/createAlipayIcon.ts --config ./scripts/config/alipay.json
 npx ts-node src/commands/createWechatIcon.ts --config ./scripts/config/wechat.json
